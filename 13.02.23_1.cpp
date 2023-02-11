c065b9d3987c5db23c85b10d6e23fbd5069e2bb9
ArT-191 <115080147+ArT-191@users.noreply.github.com>
2023-02-11 11:48:31 +0400

    Update 13.02.23_1.cpp

--- 13.02.23_1.cpp
+++ 13.02.23_1.cpp
@@ -1,38 +1,50 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
+#include <vector>
 
-void printFrequency(int arr[], int n)
-{
+void insertionSort(std::vector<int> &arr, std::vector<int> &frequency) {
+  int n = arr.size();
+  for (int i = 1; i < n; i++) {
+    int key = arr[i];
+    int keyFrequency = frequency[i];
+    int j = i - 1;
+    while (j >= 0 && frequency[j] < keyFrequency) {
+      arr[j + 1] = arr[j];
+      frequency[j + 1] = frequency[j];
+      j--;
+    }
+    arr[j + 1] = key;
+    frequency[j + 1] = keyFrequency;
+  }
+}
 
-std::map<int, int> frequencyMap;
+int main() {
+  std::vector<int> arr = {3, 6, 3, 9, 6, 7, 9, 9};
+  int n = arr.size();
 
-for (int i = 0; i < n; i++){
+  std::unordered_map<int, int> frequencyMap;
+  for (int i = 0; i < n; i++) {
     frequencyMap[arr[i]]++;
   }
 
-for (int i = 1; i < n; i++){
-    
-    int key = arr[i];
-    int j = i - 1;
-      while (j >= 0 && frequencyMap[arr[j]] < frequencyMap[key]){
-        arr[j + 1] = arr[j];
-        j = j - 1;
-      }
+  std::vector<int> frequency(n);
+  int index = 0;
+  for (const auto &kv : frequencyMap) {
+    arr[index] = kv.first;
+    frequency[index] = kv.second;
+    index++;
+  }
 
-    arr[j + 1] = key;
-}
+  insertionSort(frequency, arr);
 
-for (int i = 0; i < n; i++){
-    std::cout << arr[i] << " ";
+  for (int i = n - 1; i >= 0; i--) {
+    for (int j = 0; j < frequency[i]; j++) {
+      std::cout << arr[i] << " ";
     }
-}
-
-int main(){
-  int arr[] = {3, 6, 3, 9, 6, 7, 9, 9};
-  int n = sizeof(arr) / sizeof(arr[0]);
-
-  printFrequency(arr, n);
+  }
+  std::cout << std::endl;
 
   return 0;
 }
+
 
