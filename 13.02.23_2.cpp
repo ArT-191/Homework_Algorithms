bd10ff382f1a51a03dc83c6139031cb317fac858
ArT-191 <115080147+ArT-191@users.noreply.github.com>
2023-02-13 23:13:45 +0400

    Add files via upload

---
+++ 13.02.23_2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int PeekFinder(int arr[], int left, int right) {
+    int mid = (left + right) / 2;
+    if (mid > 0 && arr[mid - 1] > arr[mid]) {
+        return PeekFinder(arr, left, mid - 1);
+    } else if (mid < right && arr[mid + 1] > arr[mid]) {
+        return PeekFinder(arr, mid + 1, right);
+    } else {
+        return mid;
+    }
+}
+
+int main() {
+    int arr[] = {1, 3, 20, 4, 1, 0};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int peakIndex = PeekFinder(arr, 0, n - 1);
+    std::cout << arr[peakIndex];
+    return 0;
+}
