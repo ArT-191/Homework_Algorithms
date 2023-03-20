171c7155992fda35e033b09a09e07f6ca255a1bf
ArT-191 <115080147+ArT-191@users.noreply.github.com>
2023-03-21 00:29:48 +0400

    Add files via upload

---
+++ 20.03.23_1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+
+void sumOfUnitFractions(int a, int b) {
+    if (a >= b || a == 0) {
+        std::cout << "Invalid input: numerator should be positive and less than denominator\n";
+        return;
+    }
+    
+    std::vector<int> denominators; 
+    
+    while (a != 0) {
+        int d = (b + a - 1) / a; 
+        
+        denominators.push_back(d);
+        
+        a = a * d - b;
+        b = b * d;
+        
+        // check for repeated denominators and remove them
+        auto it = unique(denominators.begin(), denominators.end());
+        denominators.resize(distance(denominators.begin(), it));
+    }
+    
+    // print the sum of unique fractions
+    std::cout << "Sum of unique fractions with numerator 1:\n";
+    for (int i = 0; i < denominators.size(); i++) {
+        if (i == denominators.size() - 1) {
+            std::cout << "1/" << denominators[i] << std::endl;
+        } else {
+            std::cout << "1/" << denominators[i] << " + ";
+        }
+    }
+}
+
+int main() {
+    
+    int a,b; 
+    std::cin>>a>>b;
+    sumOfUnitFractions(a, b); // call the function
+    
+    return 0;
+}
